2a295116fabbafc7a5862f74a7e132324c9a2c63
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-10-05 21:05:30 +0100

    a

--- game parts/room/room.gd
+++ game parts/room/room.gd
@@ -6,21 +6,24 @@ class_name Room extends Area2D
 
 var hover := false
 var selected := false
+var mouse_offset := Vector2(0,0)
 
 func _process(delta):
 	if hover:
 		if Input.is_action_just_pressed("mouse_select"):
 			selected = true
 			Global.dragging = true
+			mouse_offset = get_global_mouse_position() - global_position
 	if Input.is_action_just_released("mouse_select"):
 		selected = false
 		Global.dragging = false
 	if selected:
-		global_position = get_global_mouse_position()
+		global_position = get_global_mouse_position() - mouse_offset
 
 func _on_mouse_entered():
 	if not Global.dragging:
 		hover = true
 
 func _on_mouse_exited():
-	hover = false
+	if not Global.dragging:
+		hover = false
