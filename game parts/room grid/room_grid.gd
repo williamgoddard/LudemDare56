e7625e7b45511505527c88e4ada522e2147bcc11
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-10-06 00:05:40 +0100

    fixed aeglens code

--- game parts/room grid/room_grid.gd
+++ game parts/room grid/room_grid.gd
@@ -39,59 +39,54 @@ func remove_old_grid_squares():
 		if node is GridSquare:
 			node.queue_free()
 
-
-
 func find_shortest_path(start_square : GridSquare, end_square : GridSquare):
 	var start_pos = start_square.grid_pos
 	var end_pos = end_square.grid_pos
 	var directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, -1), Vector2(0, 1)]
-
-	if start_pos == null or end_pos == null:
+	
+	if start_pos == null or end_pos == null or start_square.room == null or end_square.room == null:
 		return []
-	if start_square.room == null or end_square.room == null:
-		print("start or end room was null you IDIOT")
-		return []
-
+	
 	var queue = []
 	var came_from = {}
 	queue.append(start_pos)
 	came_from[start_pos] = null
-
+	
 	while queue.size() > 0:
 		var current = queue.pop_front()
-
+		
 		if current == end_pos:
 				var path = []
 				while current != null:
 					path.insert(0, current)
 					current = came_from[current]
 				return path
-
+		
 		for direction in directions:
 			var neighbor = current + direction
 			if is_valid_position(neighbor) and neighbor not in came_from:
-				var neighbor_room = grid[neighbor.x][neighbor.y]
-				if neighbor_room == null:
+				var neighbor_square = grid[neighbor.x][neighbor.y]
+				if neighbor_square == null or neighbor_square.room == null:
 					continue
 				elif direction.x == 1:
 					if !start_square.room.right_door:
 						continue
-					elif !neighbor_room.left_door:
+					elif !neighbor_square.room.left_door:
 						continue
 				elif direction.x == -1:
 					if !start_square.room.left_door:
 						continue
-					elif !neighbor_room.right_door:
+					elif !neighbor_square.room.right_door:
 						continue
 				elif direction.y == -1:
 					if !start_square.room.room_type == Global.RoomType.LADDER:
 						continue
 				elif direction.y == 1:
-					if !neighbor_room.room_type == Global.RoomType.LADDER:
+					if !neighbor_square.room.room_type == Global.RoomType.LADDER:
 						continue
 				queue.append(neighbor)
 				came_from[neighbor] = current
-
+	
 	#no path found
 	return []
 
