5a4b42b44bb441eff986ad8ddd771754430d378d
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-10-06 19:13:39 +0100

    started moving characters between rooms

--- test_scene.gd
+++ test_scene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const ROOM = preload("res://game parts/room/room.tscn")
+const CHARACTER = preload("res://game parts/character/character.tscn")
 
 @onready var room_grid = $RoomGrid
 
@@ -9,24 +10,43 @@ func _ready():
 	var room
 	
 	room = ROOM.instantiate()
+	room.left_door = true
+	room.right_door = true
+	room.room_type = Global.RoomType.LADDER
 	add_child(room)
 	room_grid.grid[0][1].room = room
 	room.position = Vector2(0,0)
 	
 	room = ROOM.instantiate()
+	room.left_door = true
+	room.right_door = true
+	room.room_type = Global.RoomType.LADDER
 	add_child(room)
 	room_grid.grid[0][2].room = room
 	room.position = Vector2(0,0)
 	
 	room = ROOM.instantiate()
+	room.left_door = true
+	room.right_door = true
+	room.room_type = Global.RoomType.LADDER
 	add_child(room)
-	room_grid.grid[4][1].room = room
+	room_grid.grid[1][2].room = room
 	room.position = Vector2(0,0)
 	
 	room = ROOM.instantiate()
+	room.left_door = true
+	room.right_door = true
+	room.room_type = Global.RoomType.LADDER
 	add_child(room)
-	room_grid.grid[4][2].room = room
+	room_grid.grid[2][2].room = room
 	room.position = Vector2(0,0)
+	
+	var character = CHARACTER.instantiate()
+	add_child(character)
+	room_grid.grid[0][1].room.add_character(character)
+	var path = room_grid.find_shortest_path(room_grid.grid[0][1], room_grid.grid[2][2])
+	character.position = Vector2(0,0)
+	character.execute_path(path)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
