c59c6cabe1ebe2aa43207d28d11a5d5a0e877783
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-10-06 21:05:30 +0100

    added room backgrouinds

--- game parts/room/room.gd
+++ game parts/room/room.gd
@@ -3,15 +3,49 @@ class_name Room extends Area2D
 signal room_been_moved()
 signal character_left_room()
 
-@export var left_door := false
-@export var right_door := false
-@export var room_type := Global.RoomType.DEFAULT
+@onready var room_border = $RoomBorder
+@onready var room_background = $RoomBackground
+
+const BORDER = preload("res://game parts/room/sprites/border.png")
+const BORDER_BOTH = preload("res://game parts/room/sprites/border_both.png")
+const BORDER_LEFT = preload("res://game parts/room/sprites/border_left.png")
+const BORDER_RIGHT = preload("res://game parts/room/sprites/border_right.png")
+
+const DEFAULT_1 = preload("res://game parts/room/sprites/default_1.png")
+const DEFAULT_2 = preload("res://game parts/room/sprites/default_2.png")
+const DEFAULT_3 = preload("res://game parts/room/sprites/default_3.png")
+const BEDROOM = preload("res://game parts/room/sprites/bedroom.png")
+const BATHROOM = preload("res://game parts/room/sprites/bathroom.png")
+const KITCHEN = preload("res://game parts/room/sprites/kitchen.png")
+const LADDER = preload("res://game parts/room/sprites/ladder.png")
+
+@export var left_door := false:
+	set(value):
+		left_door = value
+		if is_ready:
+			update_border()
+@export var right_door := false:
+	set(value):
+		right_door = value
+		if is_ready:
+			update_border()
+@export var room_type := Global.RoomType.DEFAULT:
+	set(value):
+		room_type = value
+		if is_ready:
+			update_background()
 
 var hover := false
 var selected := false
 var mouse_offset := Vector2(0,0)
 var overlap_areas = []
 var characters = []
+var is_ready = false
+
+func _ready():
+	update_border()
+	update_background()
+	is_ready = true
 
 func _process(delta):
 	if hover and Input.is_action_just_pressed("mouse_select") and not Global.dragging:
@@ -62,3 +96,26 @@ func add_character(character: GameCharacter):
 func remove_character(character: GameCharacter):
 	characters.erase(character)
 	character.movement_finished.disconnect(_on_character_leave)
+
+func update_border():
+	if not left_door and not right_door:
+		room_border.texture = BORDER
+	elif left_door and not right_door:
+		room_border.texture = BORDER_LEFT
+	elif not left_door and right_door:
+		room_border.texture = BORDER_RIGHT
+	else:
+		room_border.texture = BORDER_BOTH
+
+func update_background():
+	match room_type:
+		Global.RoomType.DEFAULT:
+			room_background.texture = [DEFAULT_1, DEFAULT_2, DEFAULT_3][randi_range(0,2)]
+		Global.RoomType.LADDER:
+			room_background.texture = LADDER
+		Global.RoomType.BATHROOM:
+			room_background.texture = BATHROOM
+		Global.RoomType.BEDROOM:
+			room_background.texture = BEDROOM
+		Global.RoomType.KITCHEN:
+			room_background.texture = KITCHEN
